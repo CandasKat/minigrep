2eee39930037e31403a06b979ea1f9d9e767eea5
Candas Kat <hrisohest@gmail.com>
2024-01-18 11:34:29 +0100

    Phase 2:
    Implémenter la méthode "search_in_file" (recherche un mot dans un fichier) et handle_args (qui va handler les arguments qu'on passe)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,27 @@
+use std::fs;
+
 pub fn search_in_file(filename: &str, query: &str) -> String {
-    format!("")
+    if let Ok(contents) = fs::read_to_string(filename) {
+        if contents.contains(query) {
+            return format!("'{}' found in {}", query, filename)
+        }
+        else {
+            return format!("'{}' not found in {}", query, filename)
+        }
+        return format!("'{}' found in {}", query, filename)
+    }
+    else {
+        return format!("")
+    }
 }
 
 pub fn handle_args(args: &[String]) -> String {
-    format!("")
-}
+    let filename = &args[1];
+    let query = &args[2];
 
+    let result = search_in_file(filename, query);
+    return result;
+}
 
 #[cfg(test)]
 mod test {
