510a5911249f7bf8e19a03d389212dd3ffa1c842
edgar <EdgarAlves@outlook.fr>
2024-01-25 10:17:56 +0100

    Phase 2:
    Corriger erreurs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use minigrep::handle_args;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    match handle_args(&args) {
-        Ok(message) => println!("{}", message),
-        Err(e) => println!("Erreur : {}", e),
-    }
+    let result = handle_args(&args);
+    println!("{}", result)
 }
