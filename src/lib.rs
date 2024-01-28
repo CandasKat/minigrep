b0df1b0d47dbc64f5cd6166b558b8fa16304491c
Candas Kat <hrisohest@gmail.com>
2024-01-28 16:31:29 +0100

    R: Test écrit si le mot recherché ne fait pas partie des arguments valides saisis dans le fichier.

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,12 @@ mod test{
     }
 
     #[test]
+    fn handle_args_with_valid_args_not_find_words() {
+        let args = vec![String::from("minigrep"), String::from("test.txt"), String::from("Python")];
+        assert_eq!(handle_args(&args), "'Python' not found in test.txt");
+    }
+
+    #[test]
     fn find_word_in_file() {
         let filename = "test.txt";
         let query = "rust";
