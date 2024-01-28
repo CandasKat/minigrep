15f54f3fd0e0ccdfdb35d23a939acec12b1a666e
Candas Kat <hrisohest@gmail.com>
2024-01-28 15:43:09 +0100

    R: Créer un scénario de test pour les arguments à donner dans le terminal

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,20 @@ pub fn search_in_file(filename: &str, query: &str) -> bool {
     }
     return false;
 }
+
+pub fn handle_args(args: &[String]) -> String {
+    format!("")
+}
+
 #[cfg(test)]
 mod test{
     use super::*;
+
+    #[test]
+    fn handle_args_with_valid_args() {
+        let args = vec![String::from("minigrep"), String::from("test.txt"), String::from("rust")];
+        assert_eq!(handle_args(&args), "'rust' found in test.txt");
+    }
 
     #[test]
     fn find_word_in_file() {
@@ -18,4 +29,6 @@ mod test{
         let query = "rust";
         assert!(search_in_file(filename, query));
     }
+
+
 }
